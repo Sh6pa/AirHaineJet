d54b3f33f72e3895b4a693341593e67750ac43ce
R6Coach <barthelemy.mahe.pro@gmail.com>
2023-12-06 12:25:35 +0100

    narrowDown

--- Assets/Script/Chest.cs
+++ Assets/Script/Chest.cs
@@ -2,16 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using static UnityEditor.Progress;
 
 public class Chest : MonoBehaviour
 {
     [SerializeField] public  int _number;
-    [SerializeField] public string _name;
+    [SerializeField] public string _item;
+    [SerializeField] public bool _isOpen;
+    [SerializeField] public bool _isLock;
+    [SerializeField] public string _itemToUnlock;
+    [SerializeField] public bool _isItemTaken;
 
-    public void OpenChest()
+
+    public void TryOpenChest()
     {
-        Debug.Log("chest ouvert");
+        if (GameManager._instance._listOwnItem.Contains(_itemToUnlock))
+        {
+            Debug.Log("chest ouvert");
+            _isOpen = true;
+        }
+        else
+        {
+            Debug.Log("Item Manquant");
+        }
     }
-
-
+    public void InteractChest()
+    {
+        if(_isOpen == false)
+        {
+            TryOpenChest();
+        }
+        else if(_isItemTaken  == false)
+        {
+            TakeItem();
+        }
+    }
+    public void TakeItem()
+    {
+        _isItemTaken = true;
+        GameManager._instance._listOwnItem.Add(_item);
+        Debug.Log("Item " + _item + " est récuperer");
+    }
 }
--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,24 +1,75 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    static GameManager _instance;
-    [SerializeField] Chest ChestPrefab;
-    [SerializeField] GameObject Grid;
-    [SerializeField] List<string> listItem;
-    [SerializeField] int _chestQuantity;
+    public static GameManager _instance;
+    [SerializeField] public Chest ChestPrefab;
+    [SerializeField] public GameObject Grid;
+    [SerializeField] public List<string> _listOwnItem;
+    [SerializeField] public Dictionary<string, Chest> _dictChest;
+    [SerializeField] public int _chestQuantity;
+    [SerializeField] public bool _useRandomSeed;
+    [SerializeField] public string _seed;
+    [SerializeField, Range(0,100)] public int _lockRate;
 
     public void SpawnChest()
     {
-        for(int i = 0; i < _chestQuantity; i++)
+        _dictChest.Clear();
+        _listOwnItem.Clear();
+        System.Random generator = GetRandom();
+        for (int i = 0; i < _chestQuantity; i++)
         {
+            int random = generator.Next(0, 100);
             Chest chest = Instantiate(ChestPrefab, Grid.transform);
             chest._number = i;
+            chest._item = ((char)i+65).ToString();
+            _dictChest.Add(chest._item, chest);
+            if(random < _lockRate)
+            {
+                chest._isLock = true;
+            }
         }
+        AddLockItem(generator);
     }
     private void Start()
     {
         SpawnChest();
+    }
+
+    private System.Random GetRandom()
+    {
+        if (_useRandomSeed)
+        {
+            _seed = (DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond).ToString();
+        }
+        return new System.Random(_seed.GetHashCode());
+    }
+
+    public void AddLockItem(System.Random generator)
+    {
+        List<string> itemsList = _dictChest.Keys.ToList<string>();
+        foreach (var (item,chest) in _dictChest)                
+        {
+            if (chest._isLock)
+            {
+                chest._itemToUnlock = itemsList[generator.Next(0, itemsList.Count - 1)];
+            }
+        }
+    }
+    public void NarrowDownItem()
+    {
+
+    }
+
+    public bool IsChainUnlock(Chest chest)
+    {
+        if(chest._isLock)
+        {
+            return IsChainUnlock(_dictChest[chest._itemToUnlock]);
+        }
+        return true;
     }
 }
