e2face2d2848dd27c6ce53dbdbc02a34500395b2
R6Coach <barthelemy.mahe.pro@gmail.com>
2023-12-06 16:59:41 +0100

    push

--- Assets/Script/Chest.cs
+++ Assets/Script/Chest.cs
@@ -7,31 +7,31 @@ using static UnityEditor.Progress;
 
 public class Chest : MonoBehaviour
 {
-    [SerializeField] public  int _number;
+    [SerializeField] public int _number;
     [SerializeField] public string _item;
     [SerializeField] public bool _isOpen;
     [SerializeField] public bool _isLock;
     [SerializeField] public string _itemToUnlock;
     [SerializeField] public bool _isItemTaken;
     [SerializeField] public TextMeshProUGUI _textChest;
+    [SerializeField] public TextMeshProUGUI _nomChest;
     [SerializeField] public Animator _animator;
 
     public void Start()
     {
-        _textChest.text = " ";
+        _textChest.text = "";
     }
     public void TryOpenChest()
     {
         if (GameManager._instance._listOwnItem.Contains(_itemToUnlock) || _isLock == false)
         {
-            Debug.Log("chest ouvert");
             _animator.Play("OpeningChest");
-            _textChest.text = _item.ToString();
+            _textChest.text = "Prend l'item " + _item.ToString();
             _isOpen = true;
         }
         else
         {
-            Debug.Log("Item Manquant");
+            _textChest.text = "Item " + _itemToUnlock + " Manquant";
         }
     }
     public void InteractChest()
@@ -49,6 +49,8 @@ public class Chest : MonoBehaviour
     {
         _isItemTaken = true;
         GameManager._instance._listOwnItem.Add(_item);
+        GameManager._instance.RefreshListOwnItem();
+        _animator.Play("EmptyChest");
         Debug.Log("Item " + _item + " est récuperer");
     }
 }
--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using TMPro;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,10 +12,13 @@ public class GameManager : MonoBehaviour
     [SerializeField] public Chest ChestPrefab;
     [SerializeField] public GameObject Grid;
     [SerializeField] public List<string> _listOwnItem = new List<string>();
+    [SerializeField] public TextMeshProUGUI _ownItem;
+    [SerializeField] public TextMeshProUGUI _chestQuantityText;
     [SerializeField] public Dictionary<string, Chest> _dictChest = new Dictionary<string, Chest>();
     [SerializeField] public int _chestQuantity;
     [SerializeField] public bool _useRandomSeed;
     [SerializeField] public string _seed;
+    [SerializeField] public GridLayoutGroup _grid;
     [SerializeField, Range(0,100)] public int _lockRate;
 
     private void Awake()
@@ -29,6 +35,7 @@ public class GameManager : MonoBehaviour
             int random = generator.Next(0, 100);
             Chest chest = Instantiate(ChestPrefab, Grid.transform);
             chest._number = i;
+            chest._nomChest.text = (chest._number+1).ToString();
             chest._item = ((char)(i+65)).ToString();
             _dictChest.Add(chest._item, chest);
             if(random < _lockRate)
@@ -59,7 +66,11 @@ public class GameManager : MonoBehaviour
         {
             if (chest._isLock)
             {
-                chest._itemToUnlock = itemsList[generator.Next(0, itemsList.Count - 1)];
+                chest._itemToUnlock = chest._item;
+                //while (chest._itemToUnlock == chest._item)
+                //{
+                    chest._itemToUnlock = itemsList[generator.Next(0, itemsList.Count - 1)];
+                //}
             }
         }
     }
@@ -75,5 +86,51 @@ public class GameManager : MonoBehaviour
             return IsChainUnlock(_dictChest[chest._itemToUnlock]);
         }
         return true;
+    }
+
+    public void RegenerateChest()
+    {
+        foreach(var chest in _dictChest) {
+            Destroy(chest.Value.gameObject);
+        }
+        float cellsize = Mathf.Min(1000 / _chestQuantity,200);
+        float spacing = Mathf.Min(_chestQuantity * 3f, 55);
+        _grid.cellSize = new Vector2(cellsize, cellsize);
+        _grid.spacing = new Vector2(_grid.spacing.x, spacing);
+        SpawnChest();
+        RefreshListOwnItem();
+    }
+    public void RefreshListOwnItem()
+    {
+        _ownItem.text = "Items owned : ";
+        for (int i = 0; i < _listOwnItem.Count; i++)
+        {
+            _ownItem.text = _ownItem.text + _listOwnItem[i].ToString() + " / ";
+        }     
+    }
+    public void IncreaseChestNumber()
+    {
+        if (_chestQuantity >= 30)
+        {
+            return;
+        }
+        else
+        {
+            _chestQuantity = _chestQuantity + 1;
+            _chestQuantityText.text = _chestQuantity.ToString();
+        }
+    }
+
+    public void DecreaseChestNumber()
+    {
+        if(_chestQuantity <= 2)
+        {
+            return;
+        }
+        else
+        {
+            _chestQuantity = _chestQuantity - 1;
+            _chestQuantityText.text = _chestQuantity.ToString();
+        }
     }
 }
